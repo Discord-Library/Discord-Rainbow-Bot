54620048c3b7a53f280117b583d8f16a414ec0c5
DiscordLibrary <66747010+DiscordLibrary@users.noreply.github.com>
2020-06-12 00:41:54 +0100

    New Version of The Bot

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+title Installing The Required Modules For The Bot 
+@echo Hello there, The download will start shortly. It might take some time. Just have some patience
+timeout /t 2 >nul
+@echo Installing...
+call npm install discord.js@12.2.0 chalk@4.0.0 ntcjs@1.1.2
+cls
+@echo I've installed the required modules
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+title Starting The Bot
+@echo Starting...
+node index.js
+pause
