ecb69baa6e6cab811ff4387cbe661860152f60ba
DiscordLibrary <66747010+DiscordLibrary@users.noreply.github.com>
2020-06-14 21:05:24 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,11 @@
 @echo off
 title Starting The Bot
 @echo Starting...
+@echo Thank you for using the bot.
+@echo If you have any problem you can join to the support server [https://discord.gg/5Ka5aKx]
+timeout /t 2 >nul
+@echo.
+title Rainbow Bot By Discord Library
+@echo Started The Bot
 node index.js
-pause
+pause >nul
